a8c1e8fb1df5cc8bc124aafedc5aeedcfb02edf1
Victoria-od <vittori-88@mail.ru>
2017-02-09 14:34:55 +0200

    without slider

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){/*
+	$('.feedback-slider').slick({
+		'dots': true,
+		'arrows':false,
+		'draggable':true,
+		'speed': 500,
+		'slidesToShow': 1,
+		'slidesToScroll': 1,
+		responsive: [
+		{
+			breakpoint: 1366,
+			settings: {
+				'slidesToShow': 1,
+				'slidesToScroll': 1,
+				'dots': true
+			}
+		},
+		{
+			breakpoint: 1024,
+			settings: {
+				'slidesToShow': 1,
+				'slidesToScroll': 1,
+				'dots': true
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				'slidesToShow': 1,
+				'slidesToScroll': 1
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				'slidesToShow': 1,
+				'slidesToScroll': 1
+			}
+		}
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+    ]
+});*/
+});
